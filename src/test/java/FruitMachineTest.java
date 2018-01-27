ccc70680eab84825b458315e7258bdf74077fbee
Ross Hill <rosshill88@gmail.com>
2018-01-27 12:02:45 +0000

    Instantiated test construcotr for FruitMachine

--- src/test/java/FruitMachineTest.java
+++ src/test/java/FruitMachineTest.java
@@ -1,2 +1,15 @@
+import org.junit.Before;
+
 public class FruitMachineTest {
+
+    FruitMachine fruitMachine_win;
+    FruitMachine fruitMachine_lose;
+
+    @Before
+    public void before() {
+        fruitMachine_win = new FruitMachine(Symbol.CHERRY, Symbol.CHERRY, Symbol.CHERRY);
+        fruitMachine_win = new FruitMachine(Symbol.CHERRY, Symbol.CHERRY, Symbol.BAR);
+    }
+
+
 }
