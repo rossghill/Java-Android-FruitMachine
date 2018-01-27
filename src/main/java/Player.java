8eda53a03e5f5f2bcdbc485821975a78faedba66
Ross Hill <rosshill88@gmail.com>
2018-01-27 15:54:48 +0000

    Created Player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+
+    String player_name;
+    Double wallet;
+
+    public Player (String player_name, Double wallet) {
+        this.player_name = player_name;
+        this.wallet = wallet;
+
+    }
+}
